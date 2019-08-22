175010541fb42881287db5b8f4669b989fb3825d
Defy1x <mcdonald.cas@yahoo.com>
2019-08-22 02:16:56 -0400

    ready

--- pset-functions.js
+++ pset-functions.js
@@ -29,8 +29,41 @@ Use the following test cases to confirm your program meets the success criteria
 ************/
 console.log('Problem 1:')
 
-// Add your code below this line
+let playerCardScore = 21
+let dealerCardScore = 21
 
+const maximumScore = 21
+const overScore = 22
+const defaultScore = 0
+
+function blackJack(playerCardScore, dealerCardScore) {
+
+  let outComeOne = maximumScore - playerCardScore;
+  let outComeTwo = maximumScore - dealerCardScore;
+
+  if (playerCardScore >= overScore && dealerCardScore >= overScore){
+    return(defaultScore)
+  }
+  else if (playerCardScore >= overScore){
+    return(dealerCardScore)
+  }
+  else if (dealerCardScore >= overScore){
+    return(playerCardScore)
+  }
+  else if (outComeOne > outComeTwo) {
+    return(dealerCardScore)
+  }
+  else if (outComeTwo > outComeOne) {
+    return(playerCardScore)
+  }
+  else if (playerCardScore && dealerCardScore === 21){
+    return(maximumScore)
+  }
+
+}
+
+// Add your code below this line
+console.log(blackJack(playerCardScore, dealerCardScore))
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -91,6 +124,31 @@ console.log('Problem 2:')
 
 // Add your code below this line
 
+function wordCounter (phrase) {
+  //function called word counter with parameter of phrase that passes in the string
+     const wordCount = phrase.split(' ')
+     //split the phrase up! SPACE!!!! YOU NEED SPACE!!!! >(^.^)<
+     const wordMapObj = {} // keeps track of occurances
+// any time you have to check a string you use a hashmap
+     wordCount.forEach((word) => {
+      //check if word is already in wordMapObj
+      //if it is not in wordmapObj then add it and set value to one
+      //if word is already in wordMapObj then increment value of word (key) by 1
+
+      //how do you check to see if a key is in the object? you can use dot notation or use bracket notation as well
+      //with dot notation you cannot pass in a variable to see if that key exists
+      if (wordMapObj[word] === undefined) {
+        //if word does not exist in wordMapObj
+        wordMapObj[word] = 1
+      }
+      else {
+        wordMapObj[word] += 1
+      }
+     })
+    console.log(wordMapObj)
+}
+wordCounter('Humpty Dumpty sat on a wall Humpty Dumpty had a great fall')
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -136,6 +194,45 @@ console.log('Problem 3:')
 
 // Add your code below this line
 
+const scrabbleWord = "javascript"
+
+function scrabbleScore(scrabbleWord) {
+  const scrabbleLetters = scrabbleWord.split('')
+
+  let letterPoints = scrabbleLetters.map(letter => {
+    if (letter === 'q' || letter === 'z'){
+    return 10
+    }
+      else if (letter === 'j' || letter === 'x')
+      {
+      return 8
+      }
+      else if (letter === 'k'){
+      return 5
+      }
+       else if (letter === 'f' || letter  === 'h' || letter  === 'v' || letter  === 'w' || letter  === 'y')
+      {
+      return 4
+      }
+      else if (letter  === 'b' || letter === 'c' || letter === 'm' || letter === 'p')
+      {
+      return 3
+      }
+      else if (letter === 'd' || letter === 'g')
+      {
+      return 2
+      }
+       else {
+      return 1
+      }
+    })
+  const totalPoints = letterPoints.reduce((a,b) => a + b, 0)
+//*happy dance*
+  console.log(totalPoints)
+}
+console.log(scrabbleScore('scrabbleWord'))
+
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -174,6 +271,32 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 4:')
 
 // Add your code below this line
+/* alright (your name here). Here's what's happening.
+// string is being passed through a function with a parameter of paladinword
+// the function is splitting the word into an array
+// there is a function of p checker that is checking the if the string and the value is strict equals.
+// if it is then return the value of true, if not return false
+// THATS IT!!! WOHOO >(^.^)< */
+
+
+function isPalindrome(paladinWord) {
+  const paladinReverseArray = paladinWord.split('').reverse().join('');
+  //koo, i am getting the reverse of the string here
+  //console.log(paladinReverseArray)
+
+  function paladinChecker() {
+    if (paladinWord === paladinReverseArray) {
+        return true
+    }
+    else {
+      return false
+      }
+    }
+  console.log(paladinChecker())
+  }
+
+// *HAPPY DANCE*
+isPalindrome('run')
 
 // Add your code above this line
 
@@ -207,6 +330,36 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 5:')
 
 // Add your code below this line
+
+//okay i need to do a for in loop and then pass through result of the doublemap obj into the for loop so it can check to see if the character is repeated.
+//FUCK! OK. I GOT THIS. CAS YOU GOT THIS. YOU ARE THE SHIT. YOU ARE GOING TO BE THE BEST FUCKING PROGRAMMER IN THE WORLD.
+function doublePhrase(doubleWord) {
+  const doubleCounter = doubleWord.split('')
+  // NO SPACE!!! NO SPACE!!!!!!! ^^^^^ >(^.^)<
+  const doubleMapObj = {}
+
+  doubleCounter.forEach((char) => {
+
+   if (doubleMapObj[char] === undefined) {
+     doubleMapObj[char] = 1
+   }
+   else {
+     doubleMapObj[char] += 1
+   }
+  })
+
+  function charChecker() {
+    for (let char in doubleMapObj) {
+      if (doubleMapObj[char] > 1) {
+        return true
+      }
+    }
+    return false
+  }
+  console.log(charChecker())
+}
+
+doublePhrase('apple')
 
 // Add your code above this line
 
